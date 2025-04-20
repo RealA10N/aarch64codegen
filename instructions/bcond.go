da97d04a6117600f4a46d1db985b8e580350d868
Alon Krymgand <me@alon.kr>
2025-04-20 23:03:20 +0300

    Added the `Bcond` type and instruction

---
+++ instructions/bcond.go
@@ -0,0 +1,33 @@
+package instructions
+
+import (
+	"fmt"
+
+	"alon.kr/x/aarch64codegen/immediates"
+)
+
+type Bcond uint32
+
+func BCOND(cond immediates.Condition, offset immediates.Offset19Align4) Bcond {
+	return Bcond(
+		(0b1010100 << 24) |
+			(offset.Binary() << 5) |
+			(cond.Binary()),
+	)
+}
+
+func (i Bcond) Offset() immediates.Offset19Align4 {
+	return immediates.Offset19Align4((i >> 5) & 0x7FFFF)
+}
+
+func (i Bcond) Condition() immediates.Condition {
+	return immediates.Condition(i & 0b1111)
+}
+
+func (i Bcond) String() string {
+	return fmt.Sprintf("B.%s %s", i.Condition(), i.Offset())
+}
+
+func (i Bcond) Binary() uint32 {
+	return uint32(i)
+}
---
+++ instructions/bcond_test.go
@@ -0,0 +1,62 @@
+package instructions_test
+
+import (
+	"testing"
+
+	"alon.kr/x/aarch64codegen/immediates"
+	"alon.kr/x/aarch64codegen/instructions"
+	"github.com/stretchr/testify/assert"
+)
+
+func AssertExpectedBcondInstruction(
+	t *testing.T,
+	expected string,
+	cond immediates.Condition,
+	offset int32,
+) {
+	off19, err := immediates.NewOffset19Align4(offset)
+	assert.NoError(t, err)
+	instruction := instructions.BCOND(cond, off19)
+	AssertExpectedInstruction(t, expected, instruction)
+}
+
+func TestBcond0(t *testing.T) {
+	AssertExpectedBcondInstruction(t, "B.EQ #0", immediates.ConditionEq, 0)
+}
+
+func TestBcond1(t *testing.T) {
+	AssertExpectedBcondInstruction(t, "B.EQ #4", immediates.ConditionEq, 4)
+}
+
+func TestBcondMinus1(t *testing.T) {
+	AssertExpectedBcondInstruction(t, "B.EQ #-4", immediates.ConditionEq, -4)
+}
+
+func TestBcondMax(t *testing.T) {
+	AssertExpectedBcondInstruction(t, "B.EQ #1048572", immediates.ConditionEq, 0xFFFFC)
+}
+
+func TestBcondMin(t *testing.T) {
+	AssertExpectedBcondInstruction(t, "B.EQ #-1048576", immediates.ConditionEq, -0x100000)
+}
+
+func TestBcondAllConditions(t *testing.T) {
+	for _, cond := range []immediates.Condition{
+		immediates.ConditionEq,
+		immediates.ConditionNe,
+		immediates.ConditionCs,
+		immediates.ConditionCc,
+		immediates.ConditionMi,
+		immediates.ConditionPl,
+		immediates.ConditionVs,
+		immediates.ConditionVc,
+		immediates.ConditionHi,
+		immediates.ConditionLs,
+		immediates.ConditionGe,
+		immediates.ConditionLt,
+		immediates.ConditionGt,
+		immediates.ConditionLe,
+	} {
+		AssertExpectedBcondInstruction(t, "B."+cond.String()+" #0", cond, 0)
+	}
+}
