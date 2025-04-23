d2465deee60502e97a5e05dacd5df1d1a9f6b2c1
Alon Krymgand <me@alon.kr>
2025-04-24 00:01:09 +0300

    Changed exported function and type names of `sub(s)` instructions

--- instructions/sub.go
+++ instructions/sub.go
@@ -10,19 +10,25 @@ import (
 // SUB instruction with register operands
 type Sub uint32
 
-func SUB(
+func NewSubShiftedRegister(
 	Xd registers.GPRegister,
 	Xn registers.GPRegister,
 	Xm registers.GPRegister,
-	setFlags immediates.SetFlags,
 ) Sub {
 	return Sub(
 		0xCB000000 |
-			(setFlags.Binary() << 29) |
 			(Xm.Binary() << 16) |
 			(Xn.Binary() << 5) |
 			(Xd.Binary()),
 	)
+}
+
+func NewSubsShiftedRegister(
+	Xd registers.GPRegister,
+	Xn registers.GPRegister,
+	Xm registers.GPRegister,
+) Sub {
+	return NewSubShiftedRegister(Xd, Xn, Xm) | (1 << 29)
 }
 
 func (i Sub) Binary() uint32 {
@@ -49,44 +55,49 @@ func (i Sub) String() string {
 	return fmt.Sprintf("sub%s %s, %s, %s", i.SetFlags(), i.Xd(), i.Xn(), i.Xm())
 }
 
-// SUBI instruction (SUB with immediate)
-type SubImm uint32
+type SubImmediate uint32
 
-func SUBI(
+func NewSubImmediate(
 	Xd registers.GPRegister,
 	Xn registers.GPorSPRegister,
 	imm immediates.Immediate12,
-	setFlags immediates.SetFlags,
-) SubImm {
-	return SubImm(
+) SubImmediate {
+	return SubImmediate(
 		0xD1000000 |
-			(setFlags.Binary() << 29) |
 			(imm.Binary() << 10) |
 			(Xn.Binary() << 5) |
 			(Xd.Binary()),
 	)
 }
 
-func (i SubImm) Binary() uint32 {
+func NewSubsImmediate(
+	Xd registers.GPRegister,
+	Xn registers.GPorSPRegister,
+	imm immediates.Immediate12,
+) SubImmediate {
+	return NewSubImmediate(Xd, Xn, imm) | (1 << 29)
+}
+
+func (i SubImmediate) Binary() uint32 {
 	return uint32(i)
 }
 
-func (i SubImm) Xd() registers.GPRegister {
+func (i SubImmediate) Xd() registers.GPRegister {
 	return registers.GPRegister(i & 0x1F)
 }
 
-func (i SubImm) Xn() registers.GPorSPRegister {
+func (i SubImmediate) Xn() registers.GPorSPRegister {
 	return registers.GPorSPRegister((i >> 5) & 0x1F)
 }
 
-func (i SubImm) Imm() immediates.Immediate12 {
+func (i SubImmediate) Imm() immediates.Immediate12 {
 	return immediates.Immediate12((i >> 10) & 0xFFF)
 }
 
-func (i SubImm) SetFlags() immediates.SetFlags {
+func (i SubImmediate) SetFlags() immediates.SetFlags {
 	return immediates.SetFlagsFromBinary(uint32(i))
 }
 
-func (i SubImm) String() string {
+func (i SubImmediate) String() string {
 	return fmt.Sprintf("sub%s %s, %s, %s", i.SetFlags(), i.Xd(), i.Xn(), i.Imm())
 }
--- instructions/sub_test.go
+++ instructions/sub_test.go
@@ -10,102 +10,90 @@ import (
 
 func TestSub(t *testing.T) {
 	// SUB X3, X4, X5
-	AssertExpectedInstruction(t, "sub x3, x4, x5", instructions.SUB(
+	AssertExpectedInstruction(t, "sub x3, x4, x5", instructions.NewSubShiftedRegister(
 		registers.GPRegisterX3,
 		registers.GPRegisterX4,
 		registers.GPRegisterX5,
-		immediates.DoNotSetFlags,
 	))
 
 	// SUB X27, X26, X25
-	AssertExpectedInstruction(t, "sub x27, x26, x25", instructions.SUB(
+	AssertExpectedInstruction(t, "sub x27, x26, x25", instructions.NewSubShiftedRegister(
 		registers.GPRegisterX27,
 		registers.GPRegisterX26,
 		registers.GPRegisterX25,
-		immediates.DoNotSetFlags,
 	))
 }
 
 func TestSubWithXZR(t *testing.T) {
 	// Test SUB with XZR as destination
-	AssertExpectedInstruction(t, "sub xzr, x4, x5", instructions.SUB(
+	AssertExpectedInstruction(t, "sub xzr, x4, x5", instructions.NewSubShiftedRegister(
 		registers.GPRegisterXZR,
 		registers.GPRegisterX4,
 		registers.GPRegisterX5,
-		immediates.DoNotSetFlags,
 	))
 
 	// Test SUB with XZR as first source
-	AssertExpectedInstruction(t, "sub x3, xzr, x5", instructions.SUB(
+	AssertExpectedInstruction(t, "sub x3, xzr, x5", instructions.NewSubShiftedRegister(
 		registers.GPRegisterX3,
 		registers.GPRegisterXZR,
 		registers.GPRegisterX5,
-		immediates.DoNotSetFlags,
 	))
 
 	// Test SUB with XZR as second source
-	AssertExpectedInstruction(t, "sub x3, x4, xzr", instructions.SUB(
+	AssertExpectedInstruction(t, "sub x3, x4, xzr", instructions.NewSubShiftedRegister(
 		registers.GPRegisterX3,
 		registers.GPRegisterX4,
 		registers.GPRegisterXZR,
-		immediates.DoNotSetFlags,
 	))
 
 	// Multiple XZR registers
-	AssertExpectedInstruction(t, "sub xzr, xzr, xzr", instructions.SUB(
+	AssertExpectedInstruction(t, "sub xzr, xzr, xzr", instructions.NewSubShiftedRegister(
 		registers.GPRegisterXZR,
 		registers.GPRegisterXZR,
 		registers.GPRegisterXZR,
-		immediates.DoNotSetFlags,
 	))
 }
 
-func TestSubImm(t *testing.T) {
+func TestSubImmediate(t *testing.T) {
 	// SUB X3, X4, #10
-	AssertExpectedInstruction(t, "sub x3, x4, #10", instructions.SUBI(
+	AssertExpectedInstruction(t, "sub x3, x4, #10", instructions.NewSubImmediate(
 		registers.GPRegisterX3,
 		registers.GPorSPRegisterX4,
 		immediates.Immediate12(10),
-		immediates.DoNotSetFlags,
 	))
 
 	// SUB X20, SP, #1000
-	AssertExpectedInstruction(t, "sub x20, sp, #1000", instructions.SUBI(
+	AssertExpectedInstruction(t, "sub x20, sp, #1000", instructions.NewSubImmediate(
 		registers.GPRegisterX20,
 		registers.GPorSPRegisterSP,
 		immediates.Immediate12(1000),
-		immediates.DoNotSetFlags,
 	))
 }
 
-func TestSubDoSetFlags(t *testing.T) {
-	AssertExpectedInstruction(t, "subs x0, x1, x2", instructions.SUB(
+func TestSubs(t *testing.T) {
+	AssertExpectedInstruction(t, "subs x0, x1, x2", instructions.NewSubsShiftedRegister(
 		registers.GPRegisterX0,
 		registers.GPRegisterX1,
 		registers.GPRegisterX2,
-		immediates.DoSetFlags,
 	))
 
-	AssertExpectedInstruction(t, "subs xzr, x30, x29", instructions.SUB(
+	AssertExpectedInstruction(t, "subs xzr, x30, x29", instructions.NewSubsShiftedRegister(
 		registers.GPRegisterXZR,
 		registers.GPRegisterX30,
 		registers.GPRegisterX29,
-		immediates.DoSetFlags,
 	))
 }
 
-func TestSubImmDoSetFlags(t *testing.T) {
-	AssertExpectedInstruction(t, "subs x0, x1, #10", instructions.SUBI(
+func TestSubsImmediate(t *testing.T) {
+	AssertExpectedInstruction(t, "subs x0, x1, #10", instructions.NewSubsImmediate(
 		registers.GPRegisterX0,
 		registers.GPorSPRegisterX1,
 		immediates.Immediate12(10),
-		immediates.DoSetFlags,
 	))
 
-	AssertExpectedInstruction(t, "subs xzr, sp, #1000", instructions.SUBI(
+	AssertExpectedInstruction(t, "subs xzr, sp, #1000", instructions.NewSubsImmediate(
 		registers.GPRegisterXZR,
 		registers.GPorSPRegisterSP,
 		immediates.Immediate12(1000),
-		immediates.DoSetFlags,
 	))
 }
